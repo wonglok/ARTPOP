2a5ff838d401bbdf5b24c501de9652aac4f66626
Wong Lok <wonglok@wonglok.com>
2014-04-01 23:16:24 +0800

    Updates

---
+++ scripts/efe5235e.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("artpopApp",["ngCookies","ngResource","ngSanitize","ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("artpopApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
